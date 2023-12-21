bc7f1eb0f4470d52dbaf69dbd4ae486213908907
Valerian Coelho <valerian27012004@gmail.com>
2023-12-21 22:39:54 +0530

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+
+<main>
+  <div class="img"></div>
+</main>
+
+<style>
+  .img {
+    width: 500px;
+    height: 300px;
+    background-image: url('./Assets/house.jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: 100px 100px; 
+    background-color: red;
+  }
+</style>
