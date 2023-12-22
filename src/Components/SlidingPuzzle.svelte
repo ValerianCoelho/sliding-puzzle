dc500b8ffb9dde6b10552304d697b201aae990d9
Valerian Coelho <valerian27012004@gmail.com>
2023-12-22 14:18:25 +0530

    row & col indices calculated and elements displayed and translated based on index values

--- src/Components/SlidingPuzzle.svelte
+++ src/Components/SlidingPuzzle.svelte
@@ -1,11 +1,23 @@
 <script>
-  export let rows;
-  export let cols;
+  export let rows = 5;
+  export let cols = 5;
   export let src;
+
+  const rowIndices = Array.from({length: 5}, (_, index) => index + 1);
+  const colIndices = Array.from({length: 5}, (_, index) => index + 1);
 </script>
 
-<div>
-  <img src={src} width="400" alt="Sliding Puzzle Image">
+<div class="sliding-puzzle-container">
+  <!-- <img src={src} width="400" alt="Sliding Puzzle Image"> -->
+  {#each rowIndices as i}
+    {#each colIndices as j}
+      <div 
+        class="puzzle-piece" 
+        style={`transform: translate(${10 * j}px, ${10 * i}px);`}
+      >
+        Hello</div>
+    {/each}
+  {/each}
 </div>
 
 <style>
@@ -16,6 +28,15 @@
     background-size: cover;
     background-repeat: no-repeat;
     background-position: 100px 100px; 
+    background-color: red;
+  }
+  .sliding-puzzle-container {
+    position: relative;
+  }
+  .puzzle-piece {
+    position: absolute;
+    top: 0;
+    left: 0;
     background-color: red;
   }
 </style>
