b04ae4d748a1c4b4b78a22fdb378091ec35eb82b
Valerian Coelho <valerian27012004@gmail.com>
2023-12-23 16:13:10 +0530

    invalid coords access Bug Fixed

--- src/Components/SlidingPuzzle.svelte
+++ src/Components/SlidingPuzzle.svelte
@@ -14,7 +14,7 @@
   const rowIndices = Array.from({length: rows}, (_, index) => index);
   const colIndices = Array.from({length: cols}, (_, index) => index);
 
-  const coords = [];
+  $:coords = [];
 
   onMount(()=> {
     const image = new Image();
@@ -30,37 +30,42 @@
 
     for(let i=0; i<rows; i++) {
       for(let j=0; j<cols; j++) {
-        coords.push({x: i, y: j})
+        coords.push({x: j, y: i})
       }
     }
-    console.log(coords)
   })
 
   function convertCoordToIndex(i, j) {
     return i * cols + j;
   }
 
+  function calculateImgBgPosition(i, j) {
+    const backgroundPosition = `-${puzzlePieceWidth * coords[convertCoordToIndex(i, j)].x}px -${puzzlePieceHeight * coords[convertCoordToIndex(i, j)].y}px`;
+    return backgroundPosition;
+  }
+
 </script>
 
 <div class="sliding-puzzle-container">
-  {#each rowIndices as i}
-    {#each colIndices as j}
-      <div 
-        class="puzzle-piece" 
-        style={` 
-            transform: translate(${puzzlePieceWidth * j + (j * 2)}px, ${puzzlePieceHeight * i + (i * 2)}px);
-            width: ${puzzlePieceWidth}px;
-            height: ${puzzlePieceHeight}px;
-            background-image: url(${src});
-            background-size: ${imgWidth}px, ${imgHeight}px;
-            // we are trying to access the coords values before they are initialized, so its throwing an error
-            background-position: -${puzzlePieceWidth * coords[convertCoordToIndex(i, j)].x}px -${puzzlePieceHeight * coords[convertCoordToIndex(i, j)].y}px; 
-        `}
-      >
-        Hello
-      </div>
+  {#if coords.length > 0}
+    {#each rowIndices as i}
+      {#each colIndices as j}
+        <div 
+          class="puzzle-piece" 
+          style={` 
+              transform: translate(${puzzlePieceWidth * j + (j * 2)}px, ${puzzlePieceHeight * i + (i * 2)}px);
+              width: ${puzzlePieceWidth}px;
+              height: ${puzzlePieceHeight}px;
+              background-image: url(${src});
+              background-size: ${imgWidth}px, ${imgHeight}px;
+              background-position: ${calculateImgBgPosition(i, j)}; 
+          `}
+        >
+          {puzzlePieceWidth * coords[convertCoordToIndex(i, j)].x}, {puzzlePieceHeight * coords[convertCoordToIndex(i, j)].y}
+        </div>
+      {/each}
     {/each}
-  {/each}
+  {/if}
 </div>
 
 <style>
