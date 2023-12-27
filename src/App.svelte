58a353bdbf90a94c6deba80a3b5cf54f0ca70d46
Valerian Coelho <valerian27012004@gmail.com>
2023-12-27 20:08:07 +0530

    Controls Added

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,33 @@
 <script>
-  import SlidingPuzzle from "./Sliding Puzzle/SlidingPuzzle.svelte";
+  import PuzzleContainer from "./Components/PuzzleContainer.svelte";
+
+  $:rows = 3;
+  $:cols = 4;
+  $:src = "src/Assets/house.jpg"
+
+  function handleInput(source, e) {
+    if(source === 'src') {
+      src = e.target.value
+    }
+    else if(source === 'rows') {
+      rows = e.target.value
+    }
+    else if(source === 'cols') {
+      cols = e.target.value;
+    }
+  }
 </script>
 
 <main class="main">
-  <div class="puzzle-container">
-    <div class="title">D-Cipher</div>
-    <div class="outer-container">
-      <div class="inner-container">
-        <SlidingPuzzle 
-          gap = 1
-          rows = 4
-          cols = 4
-          width = 400
-          src = 'src/Assets/house.jpg'
-        />
-      </div>
-    </div>
+  <PuzzleContainer
+    rows={rows}
+    cols={cols}
+    src={src}
+  />
+  <div class="input-container">
+    <input type="text" placeholder="Enter Image Link" on:input={(e)=>handleInput('src', e)}>
+    <input type="number" placeholder="Enter Number of Rows" on:input={(e)=>handleInput('rows', e)}>
+    <input type="number" placeholder="Enter the Number of Columns" on:input={(e)=>handleInput('cols', e)}>
   </div>
 </main>
 
@@ -24,6 +36,7 @@
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    user-select: none;
   }
   .main {
     display: flex;
@@ -33,36 +46,18 @@
     height: 100vh;
     background-color: #E0E0E0;
   }
-  .title {
-    font-family: 'Inria Serif';
-    margin-right: auto;
-    display: inline-block;
-    padding: 10px;
-    color: rgba(0, 0, 0, 0.5);
-    margin-bottom: 5px;
-    border-radius: 4px;
-    box-shadow: 2px 2px 7px 0px rgba(0, 0, 0, 0.25), 
-                1px 1px 2px 0px rgba(0, 0, 0, 0.25), 
-                -1px -1px 2.1px 0px #FFF, 
-                -2px -2px 0.8px 0px #FFF;
+  .input-container {
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+    padding: 16px;
   }
-  .outer-container {
-    padding: 24px;
-    border-radius: 6px;
-    background: #E0E0E0;
-    box-shadow: 2px 2px 7px 0px rgba(0, 0, 0, 0.25), 
-                1px 1px 2px 0px rgba(0, 0, 0, 0.25), 
-                -1px -1px 2.1px 0px #FFF, 
-                -2px -2px 0.8px 0px #FFF;
-  }
-  .inner-container {
-    background-color: blue;
-    padding: 1px;
-    border-radius: 2px;
-    background: #E0E0E0;
-    box-shadow: 2px 2px 4.2px 0px #FFF, 
-                1px 1px 1.6px 0px #FFF, 
-                -1px -1px 1.3px 0px rgba(0, 0, 0, 0.25), 
-                -0.5px -0.5px 0.8px 0px rgba(0, 0, 0, 0.25);
+  input {
+    outline: none;
+    border: 1px solid rgba(0, 0, 0, 0.5);
+    background-color: #E0E0E0;
+    padding: 10px 5px;
+    width: 450px;
+    border-radius: 5px;
   }
 </style>
---
+++ src/Components/PuzzleContainer.svelte
@@ -0,0 +1,58 @@
+<script>
+    import SlidingPuzzle from "../Sliding Puzzle/SlidingPuzzle.svelte";
+    export let rows;
+    export let cols;
+    export let src;
+  </script>
+  
+  <div class="puzzle-container">
+    <div class="title">D-Cipher</div>
+    <div class="outer-container">
+        <div class="inner-container">
+          <SlidingPuzzle 
+            gap = 1
+            rows = {rows}
+            cols = {cols}
+            width = 400
+            src = {src}
+          />
+        </div>
+    </div>
+  </div>
+  
+  <style>
+    .title {
+      font-family: 'Inria Serif';
+      margin-right: auto;
+      display: inline-block;
+      padding: 10px;
+      color: rgba(0, 0, 0, 0.5);
+      margin-bottom: 5px;
+      border-radius: 4px;
+      box-shadow: 2px 2px 7px 0px rgba(0, 0, 0, 0.25), 
+                  1px 1px 2px 0px rgba(0, 0, 0, 0.25), 
+                  -1px -1px 2.1px 0px #FFF, 
+                  -2px -2px 0.8px 0px #FFF;
+    }
+    .outer-container {
+      padding: 24px;
+      border-radius: 6px;
+      background: #E0E0E0;
+      box-shadow: 2px 2px 7px 0px rgba(0, 0, 0, 0.25), 
+                  1px 1px 2px 0px rgba(0, 0, 0, 0.25), 
+                  -1px -1px 2.1px 0px #FFF, 
+                  -2px -2px 0.8px 0px #FFF;
+    }
+    .inner-container {
+      background-color: blue;
+      padding: 1px;
+      border-radius: 2px;
+      background: #E0E0E0;
+      box-shadow: 2px 2px 4.2px 0px #FFF, 
+                  1px 1px 1.6px 0px #FFF, 
+                  -1px -1px 1.3px 0px rgba(0, 0, 0, 0.25), 
+                  -0.5px -0.5px 0.8px 0px rgba(0, 0, 0, 0.25);
+    }
+  </style>
+  
+  
