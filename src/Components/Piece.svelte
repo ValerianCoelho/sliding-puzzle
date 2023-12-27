0441399c1518ce00adbb2daf4bcb49c5d9345d6c
Valerian Coelho <valerian27012004@gmail.com>
2023-12-27 16:01:03 +0530

    Box Shadow Added to Pieces

--- src/Components/Piece.svelte
+++ src/Components/Piece.svelte
@@ -9,7 +9,7 @@
   export let puzzlePieceWidth;
   export let puzzlePieceHeight;
 
-  $:Translation = `translate(${puzzlePieceWidth * translation.x + (translation.x * 2)}px, ${puzzlePieceHeight * translation.y + (translation.y * 2)}px)`
+  $:Translation = `translate(${puzzlePieceWidth * translation.x + (translation.x * 1)}px, ${puzzlePieceHeight * translation.y + (translation.y * 1)}px)`
   $:Position = `-${puzzlePieceWidth * position.x}px -${puzzlePieceHeight * position.y}px`;
 
 </script>
@@ -36,5 +36,8 @@
     background-color: red;
     color: white;
     transition: all 0.2s ease-in-out;
+    border-radius: 8%;
+    background: #e0e0e0;
+    box-shadow:  1px 1px 2px rgba(0, 0, 0, 0.5);
   }
 </style>
